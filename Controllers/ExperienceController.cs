3bde8ae043a8852019093a45e4017cf723a73f14
eliffayydnnn <eliffayydnnn15@gmail.com>
2026-01-05 13:49:21 +0300

    ilk yukleme

---
+++ Controllers/ExperienceController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        MyPortfolioContext portfolioContext= new MyPortfolioContext();
+        public IActionResult ExperienceList()
+        {
+            var values=portfolioContext.Experiences.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            portfolioContext.Experiences.Add(experience);
+            portfolioContext.SaveChanges();
+            return RedirectToAction("ExperienceList"); 
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var values = portfolioContext.Experiences.Find(id);
+            portfolioContext.Experiences.Remove(values);
+            portfolioContext.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var values = portfolioContext.Experiences.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            portfolioContext.Experiences.Update(experience);
+            portfolioContext.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+    }
+}
---
+++ ViewComponents/LayoutViewComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MyPortfolio.ViewComponents.LayoutViewComponents
+{
+    public class _LayoutSidebarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
